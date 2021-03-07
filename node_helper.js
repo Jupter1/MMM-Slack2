0d3330622b836edd1d7c56683e345c2717eb4dce
Jupter1 <39801700+Jupter1@users.noreply.github.com>
2021-03-07 15:09:58 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -5,50 +5,51 @@ var messages = [];
 
 
 module.exports = NodeHelper.create({
-  
-    start: function() {
-		      console.log('Starting node helper for: ' + this.name);
-	  },
+	
+	start: function() {
+		console.log('Starting node helper for: ' + this.name);
+	},
+	
+	socketNotificationReceived: function(notification, payload) {
+		if (notification === 'START_CONNECTION') {
+			this.startSlackConnection(payload.config);
+		}
+	},
 
-    socketNotificationReceived: function(notification, payload) {
-		    if (notification === 'START_CONNECTION') {
-			    this.startSlackConnection(payload.config);
-		    }
-	  },
-
-    startSlackConnection: function(config) {
-        var self = this;
-        var token = config.slackToken;
-      
-        var slackMessages
-      
-        var client = new WebClient(config.slackToken, {
-          logLevel: LogLevel.DEBUG
-        });
-      
-        const result = client.conversations.history({
-          channel: config.slackChannel
-        });
-      
-        var slackMessages = [];
-        
-        result.messages.forEach(function(message) {
-          if(!message.subtype) {
-             var slackMessage = {
-               'messageId': message.ts,
-               'user': client.users.info({ user: message.user }),
-               'message': message.text
-             };
-             slackMessages.push(slackMessage);
-          }
-        });
-      this.messages = slackMessages;
-      
-      this.broadcastMessage();
-    },
-  
-    broadcastMessage: function() {
-      this.sendSocketNotification('SLACK-DATA', this.messages);
-    }
-  
+	startSlackConnection: function(config) {
+		var self = this;
+		var token = config.slackToken;
+		
+		var slackMessages;
+		
+		var client = new WebClient(config.slackToken, {
+			logLevel: LogLevel.DEBUG
+		});
+		
+		var conversationHistory;
+		const result = client.conversations.history({
+			channel: config.slackChannel
+		});
+		conversationHistory = result.messages;
+		console.log(conversationHistory + "Nachrichten gefunden");
+		
+		var slackMessages = [];
+		conversationHistory.forEach(function(message) {
+			if(!message.subtype) {
+				var slackMessage = {
+					'messageId': message.ts,
+					'user': client.users.info({ user: message.user }),
+					'message': message.text
+				};
+				slackMessages.push(slackMessage);
+			}
+		});
+		this.messages = slackMessages;
+		
+		this.broadcastMessage();
+	},
+	
+	broadcastMessage: function() {
+      		this.sendSocketNotification('SLACK-DATA', this.messages);
+    	}
 });
