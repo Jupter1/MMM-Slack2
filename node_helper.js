32a28b7d41917bc2cbce3ac2a42f8aed5bcf6393
Jupter1 <39801700+Jupter1@users.noreply.github.com>
2021-03-07 17:43:39 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -65,9 +65,16 @@ module.exports = NodeHelper.create({
 		var slackMessages = [];
 		result.messages.forEach(function(message) {
 			if(!message.subtype) {
+				var user;
+				try {
+					user = await client.users.info({ user: message.user });
+				}
+				catch (error) {
+					console.error(error);
+				}
 				var slackMessage = {
 					'messageId': message.ts,
-					'user': client.users.info({ user: message.user }),
+					'user': user,
 					'message': message.text
 				};
 				slackMessages.push(slackMessage);
